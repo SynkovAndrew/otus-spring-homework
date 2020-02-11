1c460f0f77ecc3099c12942dff7563f87f7e61fa
Andrew Synkov <andrey.synkov@smartbics.com>
2020-02-11 16:03:06 +0300

    FileSystemNotFoundException's been fixed

--- hw01-context-xml/src/main/java/com/otus/spring/hw01/beans/ReaderImpl.java
+++ hw01-context-xml/src/main/java/com/otus/spring/hw01/beans/ReaderImpl.java
@@ -3,26 +3,40 @@ package com.otus.spring.hw01.beans;
 import com.opencsv.CSVReader;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ReaderImpl implements Reader {
 
     @Override
     public List<String[]> readFile(final String pathToFile) throws IOException, URISyntaxException {
-        final java.io.Reader reader = Files.newBufferedReader(
-                Paths.get(ClassLoader.getSystemResource(pathToFile).toURI()));
-        final var csvReader = new CSVReader(reader);
+        final URI uri = getClass().getClassLoader().getResource(pathToFile).toURI();
         final var lines = new ArrayList<String[]>();
-        String[] line;
-        while ((line = csvReader.readNext()) != null) {
-            lines.add(line);
+        try (final FileSystem fileSystem = FileSystems.newFileSystem(uri, getEnvironment())) {
+            try (final java.io.Reader reader = Files.newBufferedReader(Paths.get(uri))) {
+                try (final var csvReader = new CSVReader(reader)) {
+                    String[] line;
+                    while ((line = csvReader.readNext()) != null) {
+                        lines.add(line);
+                    }
+                }
+            }
+
         }
-        reader.close();
-        csvReader.close();
         return lines;
+    }
+
+    private Map<String, String> getEnvironment() {
+        final var env = new HashMap<String, String>();
+        env.put("create", "true");
+        return env;
     }
 }
